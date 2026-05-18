0f70f8c56ea8c4a54dc87c40fcded98b57ad36c9
av <soft@vhlv.ru>
2026-05-18 22:52:33 +0300

    correct duplicate message. Closes #2

--- storage.py
+++ storage.py
@@ -37,7 +37,7 @@ def add_book(books: List[Book], author: str, title: str, rating: int,
              date_read: Optional[str] = None) -> tuple[bool, str]:
     for book in books:
         if book.author.lower() == author.lower() and book.title.lower() == title.lower():
-            return False, f"Книга '{title}' автора {author} уже существует! Добавьте другую книгу."
+            return False, f"Книга '{title}' автора {author} уже существует!"
 
     try:
         if not date_read:
